69e07a1dc58a9448f3d1549bde3388b8c670f11d
Andreas Lefdalsnes <andreas.lefdalsnes@outlook.com>
2017-08-08 02:42:02 +0200

    Finish Adding Comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,10 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])        
+        @article = Article.find(params[:id])
+
+        @comment = Comment.new
+        @comment.article_id = @article.id        
     end
 
     def destroy
